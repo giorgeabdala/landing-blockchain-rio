be5ad34d6bde20f60596bedc63df68c11dfbee80
Giorge Abdala <giorgeabdala@gmail.com>
2023-08-21 00:51:27 -0300

    corrigido tamanhos para ajustar em telas do cel.

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -3,8 +3,7 @@
 export default function Menu() {
     return (
         <div className="flex gap-2 justify-center ">
-            <div className="text-polkadot-rose font-title text-2xl ">
-                <a href="#start" className="mr-4">Início</a>
+            <div className="text-polkadot-rose font-title text-xl ">
                 <a href="#workshop" className="mr-4">Workshops</a>
                 <a href="#ticket" className="mr-4">Ingressos</a>
                 <a href="#footer" className="mr-4">Contatos</a>
