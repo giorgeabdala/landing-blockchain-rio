b16234d40842ca6e716d8da167b662eb4f139e76
Giorge Abdala <giorgeabdala@gmail.com>
2023-08-22 19:51:30 -0300

    corrigido erro ao abrir modal.

--- src/app/components/TwitterModal.tsx
+++ src/app/components/TwitterModal.tsx
@@ -30,6 +30,7 @@ export default function TwitterModal(props: TwitterModalProps) {
                     aria-labelledby="modal-title"
                     role="dialog"
                     aria-modal="true"
+                    ariaHideApp={false}
 
                 >
                     <div className="flex items-end  justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
