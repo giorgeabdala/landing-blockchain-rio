555e50c118313f130e4a422042adc0346ab71c01
Giorge Abdala <giorgeabdala@gmail.com>
2023-08-23 15:09:12 -0300

    alterado fonte da pagina de sucesso. Criado texto na página de erro.

--- src/app/checkout/sucess/page.tsx
+++ src/app/checkout/sucess/page.tsx
@@ -11,7 +11,7 @@ export default function checkout() {
                 <Image src="/logo_transparent.png" alt="logotipo da comunidade Polkadot Brasil" width={500} height={500} />
                 <h1 className="text-4xl font-bold  font-title text-polkadot-rose ">POLKADOT BRASIL</h1>
             </div>
-            <div className="flex flex-col font-body items-center font-black text-justify mt-20 gap-8 text-xl text-white">
+            <div className="flex flex-col font-body items-center font-black text-justify mt-20 gap-8 text-lg text-white">
                 <p>PARABÉNS!!!</p>
 
                 <div>
