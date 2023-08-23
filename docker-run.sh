90aadaa73d1cd791d9952449dde04ac518f49402
Giorge Abdala <giorgeabdala@gmail.com>
2023-08-23 01:53:57 -0300

    deleta container antes de rodar.
    Roda container em modo detached

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,3 @@
+docker remove blockchain-rio-landing
 docker build -t blockchain-rio-landing .
-docker run -p 3000:3000 --name blockchain-rio-landing blockchain-rio-landing
+docker run -d -p 3000:3000 --name blockchain-rio-landing blockchain-rio-landing
