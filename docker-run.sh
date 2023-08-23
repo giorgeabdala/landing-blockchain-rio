3fc87d09cde004fdf6943cb3f20cd677e686a5b2
Giorge Abdala <giorgeabdala@gmail.com>
2023-08-23 15:57:30 -0300

    add container na rede do nginx

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,4 @@
 docker stop blockchain-rio-landing
-docker remove blockchain-rio-landing
+docker rm blockchain-rio-landing
 docker build -t blockchain-rio-landing .
-docker run -d -p 3000:3000 --name blockchain-rio-landing blockchain-rio-landing
-
+docker run -d -p 3000:3000 --name blockchain-rio-landing --network nginx-proxy-manager_nginx-proxy blockchain-rio-landing
