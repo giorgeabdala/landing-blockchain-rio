fced001151eac69a71364f7bbae6d1c6c2afccfd
Giorge Abdala <giorgeabdala@gmail.com>
2023-08-31 11:10:05 -0300

    imagem blockchain rio colocada na esquerda.

--- src/app/landing/Ticket.tsx
+++ src/app/landing/Ticket.tsx
@@ -8,10 +8,10 @@ import blockrio from "/public/photos/BlockRio1.png";
 export default function Ticket() {
     return (
         <Area className="bg-background-primary">
-            <div id="ticket" className="flex flex-col md:flex-row-reverse  lg:gap-12 items-center  justify-items-center  mt-20 mb-20 ">
-                <Image className="w-auto h-auto max-w-xs" src={blockrio} alt={"logo da Blockchain Festival"} />
-                <div className="flex justify-center mt-20 md:mt-0 sm:gap-4">
-                    <div className="flex flex-col  justify-center font-body text-justify text-2xl text-zinc-100">
+            <div id="ticket" className="grid grid-cols-1 md:grid-cols-3 mt-20 mb-20 justify-items-center md:justify-items-start">
+                <Image className="w-auto h-auto max-w-xs md: items-start" src={blockrio} alt={"logo da Blockchain Festival"} />
+                <div className="flex justify-center mt-20 md:mt-0 md:col-span-2">
+                    <div className="flex flex-col  justify-center font-body text-justify leading-loose text-2xl text-zinc-100 gap-4">
 
                         <p>
                             Garanta Seu Passaporte para a Blockchain Rio Festival 2023 e tenha a chance de
@@ -21,11 +21,6 @@ export default function Ticket() {
                         <div className="flex justify-center mt-8">
                             <Modals />
                         </div>
-
-
-
-
-
                     </div>
                 </div>
             </div>
