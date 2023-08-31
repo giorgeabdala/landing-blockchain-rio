a3a3fd871d54690db669c3b367587fe21d8e2b3d
Giorge Abdala <giorgeabdala@gmail.com>
2023-08-31 10:53:02 -0300

    alterado imagem da blockchain rio

--- src/app/landing/Ticket.tsx
+++ src/app/landing/Ticket.tsx
@@ -1,9 +1,7 @@
 import Area from "@/app/components/Area";
-import ButtonDefault from "@/app/components/Button";
-import TwitterModal from "@/app/components/TwitterModal";
-import TwitterButton from "@/app/components/TwitterButton";
 import Modals from "@/app/components/Modals";
 import Image from "next/image";
+import blockrio from "/public/photos/BlockRio1.png";
 
 
 
@@ -11,7 +9,7 @@ export default function Ticket() {
     return (
         <Area className="bg-background-primary">
             <div id="ticket" className="flex flex-col md:flex-row-reverse  lg:gap-12 items-center  justify-items-center  mt-20 mb-20 ">
-                <Image className="w-auto h-auto max-w-xs" src={"/festival-rio.webp"} alt={"logo da Blockchain Festival"} width={300} height={300} />
+                <Image className="w-auto h-auto max-w-xs" src={blockrio} alt={"logo da Blockchain Festival"} />
                 <div className="flex justify-center mt-20 md:mt-0 sm:gap-4">
                     <div className="flex flex-col  justify-center font-body text-justify text-2xl text-zinc-100">
 
