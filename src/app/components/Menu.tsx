f9db4f3b8476711e43efc272cf10e627cc50f704
Giorge Abdala <giorgeabdala@gmail.com>
2023-09-02 16:09:32 -0300

    tamanho de letra do header.

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -2,8 +2,8 @@
 
 export default function Menu() {
     return (
-        <div className="flex gap-2 justify-center ">
-            <div className="text-polkadot-rose font-title text-lg sm:text-xl">
+        <div className="flex justify-center ">
+            <div className="text-polkadot-rose font-title md:text-2xl">
                 <a href="#workshop" className="mr-4">Workshops</a>
                 <a href="#ticket" className="mr-4">Ingressos</a>
                 <a href="#footer" className="mr-4">Contatos</a>
